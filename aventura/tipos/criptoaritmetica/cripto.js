a9d304ba7d418a5f798f22698e4854746ce53ff8
JJCG <jesus_carrillog@hotmail.com>
2025-09-26 19:31:53 -0600

    Update cripto.js

--- aventura/tipos/criptoaritmetica/cripto.js
+++ aventura/tipos/criptoaritmetica/cripto.js
@@ -3,26 +3,21 @@
 // Función para renderizar criptoaritmética
 function renderizarMisionCripto(data) {
     let ejerciciosHTML = '';
-    data.ejercicios.forEach((ej, index) => {
-        const operacionHTML = `
-            <div class="cripto-operacion">
-                ${ej.operacion.linea1}<br>
-                ${ej.operacion.linea2}<br>
-                <hr>
-                ${ej.operacion.resultado}
+    data.ejercicios.forEach(ej => {
+        const figurasUnicas = obtenerFigurasUnicas(ej.operacion);
+        const inputsHTML = figurasUnicas.map(figura => `
+            <div class="cripto-input-group">
+                <label>${figura}</label>
+                <input type="number" class="cripto-input" data-figura="${figura}" min="0" max="9">
             </div>
-        `;
-        
-        const respuestaHTML = ej.solucion.map(sol => 
-            `<input type="number" class="cripto-input" data-figura="${sol.figura}" placeholder="${sol.figura}" min="0" max="9">`
-        ).join('');
-        
+        `).join('');
+
         ejerciciosHTML += `
-            <div class="cripto-ejercicio" data-respuesta="${JSON.stringify(ej.solucion)}">
-                ${operacionHTML}
-                <div class="cripto-respuesta">
-                    ${respuestaHTML}
+            <div class="cripto-ejercicio-container">
+                <div class="cripto-operacion">
+                    <pre>${ej.operacion.linea1}\n${ej.operacion.linea2}\n<hr>\n${ej.operacion.resultado}</pre>
                 </div>
+                <div class="cripto-inputs">${inputsHTML}</div>
                 <div class="feedback-container"></div>
             </div>
         `;
@@ -30,26 +25,49 @@ function renderizarMisionCripto(data) {
     return ejerciciosHTML;
 }
 
+function obtenerFigurasUnicas(operacion) {
+    const figuras = new Set();
+    operacion.linea1.split('').forEach(char => {
+        if (char.match(/[A-Z]/)) figuras.add(char);
+    });
+    operacion.linea2.split('').forEach(char => {
+        if (char.match(/[A-Z]/)) figuras.add(char);
+    });
+    operacion.resultado.split('').forEach(char => {
+        if (char.match(/[A-Z]/)) figuras.add(char);
+    });
+    return Array.from(figuras);
+}
+
 // Función para calificar criptoaritmética
 function calificarMisionCripto(misionDiv, misionData) {
-    let aciertos = 0;
-    misionDiv.querySelectorAll('.cripto-ejercicio').forEach((ejDiv, index) => {
-        const solucionCorrecta = misionData.ejercicios[index].solucion;
-        let esCorrecto = true;
-        
-        solucionCorrecta.forEach(sol => {
-            const input = ejDiv.querySelector(`[data-figura="${sol.figura}"]`);
-            if (input && input.value !== sol.valor) {
-                esCorrecto = false;
-            }
-        });
-        
-        if (esCorrecto) {
-            ejDiv.querySelectorAll('.cripto-input').forEach(input => input.classList.add('correct'));
-            aciertos++;
+    let todosCorrectos = true;
+    misionDiv.querySelectorAll('.cripto-input').forEach(input => {
+        const figura = input.dataset.figura;
+        const valorUsuario = input.value;
+        const solucionCorrecta = misionData.ejercicios[0].solucion.find(s => s.figura === figura);
+
+        input.classList.remove('correcto', 'incorrecto');
+
+        if (solucionCorrecta && valorUsuario === solucionCorrecta.valor) {
+            input.classList.add('correcto');
         } else {
-            ejDiv.querySelectorAll('.cripto-input').forEach(input => input.classList.add('incorrect'));
+            input.classList.add('incorrecto');
+            todosCorrectos = false;
         }
     });
-    return aciertos;
+
+    const explicacion = todosCorrectos ? 
+        misionData.ejercicios[0].explicacion_correcta :
+        misionData.ejercicios[0].explicacion_incorrecta;
+
+    mostrarFeedbackCripto(misionDiv.querySelector('.feedback-container'), explicacion, todosCorrectos ? 'correcto' : 'incorrecto');
+
+    return todosCorrectos ? 1 : 0;
+}
+
+function mostrarFeedbackCripto(container, texto, tipo) {
+    if (container) {
+        container.innerHTML = `<div class="feedback-box ${tipo}">${texto}</div>`;
+    }
 }
