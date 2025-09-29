6ba24909992b6b34686b7d5f7237b19ca1900ac6
JJCG <jesus_carrillog@hotmail.com>
2025-09-29 12:57:40 -0600

    Update cripto.js

--- aventura/tipos/criptoaritmetica/cripto.js
+++ aventura/tipos/criptoaritmetica/cripto.js
@@ -27,15 +27,21 @@ function renderizarMisionCripto(data) {
 
 function obtenerFigurasUnicas(operacion) {
     const figuras = new Set();
-    operacion.linea1.split('').forEach(char => {
-        if (char.match(/[A-Z]/)) figuras.add(char);
+    
+    // Regex para detectar emojis y letras mayúsculas
+    const emojiRegex = /[\u{1F300}-\u{1F6FF}\u{1F900}-\u{1F9FF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}]/gu;
+    const letraRegex = /[A-Z]/g;
+    
+    [operacion.linea1, operacion.linea2, operacion.resultado].forEach(linea => {
+        // Buscar emojis
+        const emojis = linea.match(emojiRegex) || [];
+        emojis.forEach(emoji => figuras.add(emoji));
+        
+        // Buscar letras mayúsculas
+        const letras = linea.match(letraRegex) || [];
+        letras.forEach(letra => figuras.add(letra));
     });
-    operacion.linea2.split('').forEach(char => {
-        if (char.match(/[A-Z]/)) figuras.add(char);
-    });
-    operacion.resultado.split('').forEach(char => {
-        if (char.match(/[A-Z]/)) figuras.add(char);
-    });
+    
     return Array.from(figuras);
 }
 
