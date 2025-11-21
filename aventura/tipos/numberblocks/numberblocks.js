367feed7d408fee6e7a2976e830b4b13f75c1853
JJCG <jesus_carrillog@hotmail.com>
2025-11-20 23:03:25 -0600

    Delete aventura directory

--- aventura/tipos/numberblocks/numberblocks.js
+++
@@ -1,6 +0,0 @@
-/* ===== FUNCIONES ESPECÍFICAS PARA NUMBERBLOCKS DIBUJO ===== */
-
-// Función para renderizar numberblocks dibujo
-function renderizarMisionDibujo(data) {
-    return `<p class="numberblocks-instruccion">${data.instruccion}</p><div class="canvas-placeholder">El lienzo de dibujo aparecerá aquí.</div>`;
-}
